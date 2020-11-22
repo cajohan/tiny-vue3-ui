4868fadc2c0e533a4640ccf43100a56a71eeb363
huangdi <huangdi_c@163.com>
2020-11-22 21:47:08 +0800

    Dialog 支持 visible

--- src/lib/Dialog.vue
+++ src/lib/Dialog.vue
@@ -1,25 +1,33 @@
 <template>
-  <div class="tiny-dialog-overlay"></div>
-  <div class="tiny-dialog-wrapper">
-    <div class="tiny-dialog">
-      <header>标题<span class="tiny-dialog-close"></span></header>
-      <main>
-        <p>第一行字</p>
-        <p>第二行字</p>
-        <slot/>
-      </main>
-      <footer>
-        <Button level="main">OK</Button>
-        <Button>Cancel</Button>
-      </footer>
-    </div>
+  <template v-if="visible">
+    <div class="tiny-dialog-overlay"></div>
+    <div class="tiny-dialog-wrapper">
+      <div class="tiny-dialog">
+        <header>标题<span class="tiny-dialog-close"></span></header>
+        <main>
+          <p>第一行字</p>
+          <p>第二行字</p>
+        </main>
+        <footer>
+          <Button level="main">OK</Button>
+          <Button>Cancel</Button>
+        </footer>
+      </div>
 
-  </div>
+    </div>
+  </template>
+
 </template>
 <script lang="ts">
 import Button from './Button.vue';
 
 export default {
+  props:{
+    visible: {
+      type: Boolean,
+      default: false,
+    }
+  },
   components: {
     Button,
   },
