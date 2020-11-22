a372c7d7e7bd6011def80e983cf801d6bf622b25
huangdi <huangdi_c@163.com>
2020-11-22 15:21:20 +0800

    添加 Switch 组件

--- src/App.vue
+++ src/App.vue
@@ -10,7 +10,7 @@ export default {
   name: 'App',
   setup() {
     const width = document.documentElement.clientWidth;
-    const menuVisible = ref(width <= 500 ? false : true);
+    const menuVisible = ref(width > 500);
     provide('menuVisible', menuVisible); // set
     router.afterEach(() => {
       if (width <= 500) {
