838be6093e039f406a7232d768d01736721651b0
huangdi <huangdi_c@163.com>
2020-11-22 22:03:15 +0800

    Dialog 支持点击关闭

--- src/lib/Dialog.vue
+++ src/lib/Dialog.vue
@@ -1,16 +1,21 @@
 <template>
   <template v-if="visible">
-    <div class="tiny-dialog-overlay"></div>
+    <div class="tiny-dialog-overlay" @click="onClickOverlay"></div>
     <div class="tiny-dialog-wrapper">
       <div class="tiny-dialog">
-        <header>标题<span class="tiny-dialog-close"></span></header>
+        <header>标题
+          <span class="tiny-dialog-close"
+                @click="close"
+          >
+
+          </span></header>
         <main>
           <p>第一行字</p>
           <p>第二行字</p>
         </main>
         <footer>
-          <Button level="main">OK</Button>
-          <Button>Cancel</Button>
+          <Button level="main" @click="ok">OK</Button>
+          <Button @click="cancel">Cancel</Button>
         </footer>
       </div>
 
@@ -26,11 +31,41 @@ export default {
     visible: {
       type: Boolean,
       default: false,
+    },
+    closeOnClickOverlay:{
+      type: Boolean,
+      default: true
+    },
+    ok:{
+      type: Function
+    },
+    cancel:{
+      type: Function
     }
   },
   components: {
     Button,
   },
+  setup(props,context){
+    const close = ()=>{
+      context.emit('update:visible', false)
+    }
+    const onClickOverlay = ()=>{
+      if (props.closeOnClickOverlay){
+        close()
+      }
+    }
+    const ok = ()=>{
+      if (props.ok?.() !== false){
+        close()
+      }
+    }
+    const cancel = ()=>{
+      context.emit('cancel')
+      close()
+    }
+    return {close,onClickOverlay,ok,cancel}
+  }
 };
 </script>
 
