9de8bc500d85de942ce0c2441d1815f192c0a2c4
huangdi <huangdi_c@163.com>
2020-11-22 17:33:52 +0800

    支持 theme 属性

--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -48,5 +48,21 @@ $radius: 4px;
   &::-moz-focus-inner{
     border: 0;
   }
+  &.tiny-theme-link{
+    border-color: transparent;
+    box-shadow: none;
+    color: $blue;
+    &:hover, &:focus{
+      color: lighten($blue, 10%);
+    }
+  }
+  &.tiny-theme-text{
+    border-color: transparent;
+    box-shadow: none;
+    color: inherit;
+    &:hover, &:focus{
+      color: darken(white, 5%);
+    }
+  }
 }
 </style>
