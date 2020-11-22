980162478005a7670104292ef0ad3caac33bbeda
huangdi <huangdi_c@163.com>
2020-11-22 18:28:35 +0800

    支持 disabled

--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -1,5 +1,5 @@
 <template>
-  <button class="tiny-button" :class="classes">
+  <button class="tiny-button" :class="classes" :disabled="disabled">
     <slot/>
   </button>
 </template>
@@ -17,13 +17,22 @@ export default {
       type: String,
       default: 'normal',
     },
+    level: {
+      type: String,
+      default: 'normal',
+    },
+    disabled: {
+      type: Boolean,
+      default: false,
+    }
   },
   setup(props) {
-    const {theme, size} = props;
+    const {theme, size, level} = props;
     const classes = computed(() => {
       return {
         [`tiny-theme-${theme}`]: theme,
         [`tiny-size-${size}`]: size,
+        [`tiny-level-${level}`]: level,
       };
     });
     return {classes};
@@ -36,6 +45,8 @@ $border-color: #d9d9d9;
 $color: #333;
 $blue: #40a9ff;
 $radius: 4px;
+$red: red;
+$grey: grey;
 .tiny-button {
   box-sizing: border-box;
   height: $h;
@@ -50,6 +61,7 @@ $radius: 4px;
   border: 1px solid $border-color;
   border-radius: $radius;
   box-shadow: 0 1px 0 fade-out(black, 0.95);
+  transition: background 250ms;
 
   & + & {
     margin-left: 8px;
@@ -88,15 +100,92 @@ $radius: 4px;
       color: darken(white, 5%);
     }
   }
+
   &.tiny-size-big {
     font-size: 24px;
     height: 48px;
     padding: 0 16px;
   }
+
   &.tiny-size-small {
     font-size: 12px;
     height: 20px;
     padding: 0 4px;
+  }
+
+  &.tiny-theme-button {
+    .tiny-level-main {
+      background: $blue;
+      color: white;
+      border-color: $blue;
+
+      &:hover,
+      &:focus {
+        background: darken($blue, 10%);
+        border-color: darken($blue, 10%);
+      }
+    }
+
+    &.tiny-level-danger {
+      background: $red;
+      border-color: $red;
+      color: white;
+
+      &:hover,
+      &:focus {
+        background: darken($red, 10%);
+        border-color: darken($red, 10%);
+      }
+    }
+  }
+
+  &.tiny-theme-link {
+    &.tiny-level-danger {
+      color: $red;
+
+      &:hover,
+      &:focus {
+        color: darken($red, 10%);
+      }
+    }
+  }
+
+  &.tiny-theme-text {
+    &.tiny-level-main {
+      color: $blue;
+
+      &:hover,
+      &:focus {
+        color: darken($blue, 10%);
+      }
+    }
+
+    &.tiny-level-danger {
+      color: $red;
+
+      &:hover,
+      &:focus {
+        color: darken($red, 10%);
+      }
+    }
+  }
+
+  &.tiny-theme-button {
+    &[disabled] {
+      cursor: not-allowed;
+      color: $grey;
+
+      &:hover {
+        border-color: $grey;
+      }
+    }
+  }
+
+  &.tiny-theme-link, &.tiny-theme-text {
+    &[disabled] {
+      cursor: not-allowed;
+      color: $grey;
+    }
   }
 }
 </style>
