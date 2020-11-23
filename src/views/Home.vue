2ceb11c139eac5d38a0382749a65a965ca4eaa33
huangdi <huangdi_c@163.com>
2020-11-23 15:43:15 +0800

    添加 feature 样式

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -12,15 +12,29 @@
       </div>
     </div>
     <div class="features">
-      <svg class="icon">
-        <use xlink:href="#icon-vue"></use>
-      </svg>
-      <svg class="icon">
-        <use xlink:href="#icon-ts"></use>
-      </svg>
-      <svg class="icon">
-        <use xlink:href="#icon-light"></use>
-      </svg>
+      <ul>
+        <li>
+          <svg class="icon">
+            <use xlink:href="#icon-vue"></use>
+          </svg>
+          <h3>基于 Vue 3</h3>
+          <p>骄傲地使用了 Vue 3 Composition API</p>
+        </li>
+        <li>
+          <svg class="icon">
+            <use xlink:href="#icon-ts"></use>
+          </svg>
+          <h3>基于 TypeScript </h3>
+          <p>源代码采用 TypeScript 书写（非严格检查）</p>
+        </li>
+        <li>
+          <svg class="icon">
+            <use xlink:href="#icon-light"></use>
+          </svg>
+          <h3>代码易读</h3>
+          <p>每个组件的源代码都极其简洁</p>
+        </li>
+      </ul>
     </div>
   </div>
 
@@ -35,39 +49,79 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-$bg:#3a9790;
+$bg: #3a9790;
 $border-radius: 24px;
-$color:#133f3f;
-.topnavAndBanner{
-  background: rgb(249,237,220);
-  background: linear-gradient(145deg, rgba(249,237,220,1) 0%, rgba(245,225,197,1) 100%);
+$color: #133f3f;
+.topnavAndBanner {
+  background: rgb(249, 237, 220);
+  background: linear-gradient(145deg, rgba(249, 237, 220, 1) 0%, rgba(245, 225, 197, 1) 100%);
   clip-path: ellipse(80% 60% at 50% 40%);
 }
-.features{
-  > svg{
-    width: 64px;
-    height: 64px;
+
+.features {
+  margin: 64px auto;
+  width: 400px;
+
+  @media (min-width: 800px) {
+    width: 800px;
   }
+  @media (min-width: 1200px) {
+    width: 1200px;
+  }
+
+  > ul {
+    display: flex;
+    flex-wrap: wrap;
+
+    > li {
+      width: 400px;
+      margin: 16px 0;
+      display: grid;
+      justify-content: start;
+      align-content: space-between;
+      grid-template-areas: "icon title" "icon text";
+      grid-template-columns: 80px auto;
+      grid-auto-rows: 1fr auto;
+
+      > svg {
+        width: 64px;
+        height: 64px;
+      }
+
+      > h3 {
+        grid-area: title;
+        font-size: 28px;
+      }
+
+      > p {
+        grid-area: text;
+      }
+    }
+  }
+
+
 }
+
 .banner {
-  color:$color;
+  color: $color;
   padding: 100px 0;
   display: flex;
   justify-content: center;
   align-items: center;
   flex-direction: column;
+
   > .actions {
     padding: 8px 0;
 
     a {
       margin: 0 8px;
       background: $bg;
-      color:white;
+      color: white;
       display: inline-block;
       padding: 8px 24px;
-      border-radius:$border-radius;
+      border-radius: $border-radius;
 
-      &:hover{
+      &:hover {
         text-decoration: none;
       }
     }
