14cad186ab77915d9b5a4ca3da2ec5f3cff85c21
huangdi <huangdi_c@163.com>
2020-11-23 21:33:04 +0800

    一键部署

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,7 +6,7 @@
         <h1>TinyUI</h1>
         <h2>一个厉害的 UI 框架</h2>
         <p class="actions">
-          <a href="https://github.com/cajohan">Github</a>
+          <a href="https://github.com/cajohan/tiny-vue3-ui">Github</a>
           <router-link to="/doc">开始</router-link>
         </p>
       </div>
